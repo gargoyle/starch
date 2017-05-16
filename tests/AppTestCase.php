e256bfa38768fc66f29f5f260de043fad57c2b03
Bram Van der Sype <bram.vandersype@gmail.com>
2017-05-16 10:41:27 +0200

    Added a base test case for functional test
    Also created some more app tests with said base case.

---
+++ tests/AppTestCase.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Starch\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Starch\App;
+use Zend\Diactoros\ServerRequestFactory;
+
+class AppTestCase extends TestCase
+{
+    /**
+     * @var App
+     */
+    protected $app;
+
+    /*
+     * Override this method in your tests with your own App, if you have one.
+     */
+    public function setUp()
+    {
+        $this->app = new App();
+    }
+
+    /**
+     * Send a mock request to the App
+     *
+     * @param  string $method
+     * @param  string $path
+     * @param  array $body
+     * @param  array $headers
+     *
+     * @return ResponseInterface
+     */
+    public function request($method, $path, $body = [], $headers = [])
+    {
+        $uri = $path;
+        $query = '';
+        if (false !== strpos($uri, '?')) {
+            [$uri, $query] = explode('?', $path);
+        }
+
+        $request = ServerRequestFactory::fromGlobals(
+            [
+                'SERVER_PROTOCOL'      => 'HTTP/1.1',
+                'REQUEST_METHOD'       => strtoupper($method),
+                'REQUEST_URI'          => $uri,
+                'QUERY_STRING'         => $query,
+                'SERVER_NAME'          => 'localhost',
+                'SERVER_PORT'          => 80,
+            ] + $headers,
+            null,
+            $body,
+            null,
+            null
+        );
+
+        return $this->app->process($request);
+    }
+
+    public function get($path, $headers = [])
+    {
+        return $this->request('GET', $path, null, $headers);
+    }
+
+    public function post($path, $body = null, $headers = [])
+    {
+        return $this->request('POST', $path, $body, $headers);
+    }
+
+    public function put($path, $body = null, $headers = [])
+    {
+        return $this->request('PUT', $path, $body, $headers);
+    }
+
+    public function patch($path, $body = null, $headers = [])
+    {
+        return $this->request('PATCH', $path, $body, $headers);
+    }
+
+    public function delete($path, $headers = [])
+    {
+        return $this->request('DELETE', $path, null, $headers);
+    }
+}
---
+++ tests/Functional/AppTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Starch\Tests\Functional;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Starch\Tests\AppTestCase;
+use Zend\Diactoros\Response;
+
+class AppTest extends AppTestCase
+{
+    public function testReturns404ResponseOnNotFound()
+    {
+        $response = $this->get('/');
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testReturns404ResponseOnMethodNotAllowed()
+    {
+        $this->app->get('/', function() {
+            return new Response();
+        });
+
+        $response = $this->post('/');
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+    public function testAcceptsMiddleware()
+    {
+        $this->app->get('/', function(ServerRequestInterface $request) {
+            $response = new Response();
+            $response->getBody()->write($request->getHeader('x-name')[0]);
+
+            return $response;
+        });
+
+        $this->app->add(function(ServerRequestInterface $request, DelegateInterface $delegate) {
+            $request = $request->withHeader('x-name', 'foo');
+
+            return $delegate->process($request);
+        });
+        $this->app->add(function(ServerRequestInterface $request, DelegateInterface $delegate) {
+            $response = $delegate->process($request);
+            $response->getBody()->write('bar');
+
+            return $response;
+        });
+
+        $response = $this->get('/');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('foobar', (string)$response->getBody());
+    }
+
+    public function testSetsRouteArguments()
+    {
+        $this->app->get('/{name}', function(ServerRequestInterface $request, $name) {
+            $response = new Response();
+            $response->getBody()->write($name);
+
+            return $response;
+        });
+
+        $response = $this->get('/foo');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('foo', (string)$response->getBody());
+    }
+
+    public function testTestCaseCanSendPost()
+    {
+        $this->app->post('/', function(ServerRequestInterface $request) {
+            $response = new Response();
+            $response->getBody()->write($request->getParsedBody()['name']);
+
+            return $response;
+        });
+
+        $response = $this->post('/', ['name' => 'foo']);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('foo', (string)$response->getBody());
+    }
+}
